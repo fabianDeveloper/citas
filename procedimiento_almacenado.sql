82fdb31fded3635e3b4634dd75fe0d7542307da5
fabianDev <72571855+fabianDeveloper@users.noreply.github.com>
2021-02-27 16:49:09 -0500

    Create procedimiento_almacenado.sql

---
+++ procedimiento_almacenado.sql
@@ -0,0 +1,22 @@
+/*
+    PROCEDIMIENTO ALMACENADO:
+    INGRESAR UN NOMBRE Y QUE DEVUELVA LAS CITAS QUE HA TENIDO
+*/
+CREATE OR REPLACE FUNCTION citas_usuario(varchar)
+    RETURNS TABLE(persona_uno varchar, persona_dos varchar, fecha date)
+	LANGUAGE PLPGSQL
+    AS
+$$
+DECLARE
+	initcap(nombre) ALIAS FOR $1;
+BEGIN
+    RETURN query SELECT usuario_nombre, usuario_dos, cita_fecha
+	FROM USUARIOCITA
+	INNER JOIN USUARIO ON usuario.usuario_id = usuariocita.usuariocita_uno
+	INNER JOIN PAREJA ON usuariocita.usuariocita_dos = pareja.id_dos
+	WHERE usuario_nombre = initcap(nombre) OR usuario_dos = initcap(nombre);
+END;
+$$
+
+-- COMPROBACIÓN
+SELECT * FROM citas_usuario('iAn')
